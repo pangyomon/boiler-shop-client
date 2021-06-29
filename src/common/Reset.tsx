792babb2d9e6f73b0a68f1a307c257e882a91e6a
softharshly <softharshly@gmail.com>
2021-06-29 18:22:03 +0900

    Add: emotion and reset style

---
+++ src/common/Reset.tsx
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+const resetStyle = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body,
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default resetStyle;
