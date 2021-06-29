27c19f02c7231f840d30f49627cd2dee4561ce66
softharshly <softharshly@gmail.com>
2021-06-30 05:47:53 +0900

    Add: emotion and ResponsiveBox

---
+++ src/components/box/ResponsiveBox.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { up } from 'styled-breakpoints';
+import { css } from '@emotion/react';
+
+const ResponsiveBox = styled.div`
+  width: 100%;
+
+  ${up('sm')} {
+    max-width: 640px;
+  }
+
+  ${up('md')} {
+    max-width: 768px;
+  }
+
+  ${up('lg')} {
+    max-width: 1024px;
+  }
+
+  ${up('xl')} {
+    max-width: 1280px;
+  }
+
+  ${up('xxl')} {
+    max-width: 1536px;
+  }
+
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default ResponsiveBox;
