538b90a56a66129a660882c1901cc55dca18c0b2
softharshly <softharshly@gmail.com>
2021-07-22 08:17:29 +0900

    Remove: Header compoennt

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import HomePage from '../views/HomePage';
-import NotFoundPage from '../views/NotFoundPage';
-import Header from '../components/header/Header';
+import HomePage from '../pages/HomePage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 const RootRouter = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Switch>
         <Route path={'/'} component={HomePage} exact={true} />
         <Route path={'*'} component={NotFoundPage} />
